5c5df12fd80e7dad069e9546196a813a16f4305b
Yap <wildlegion59@gmail.com>
2023-07-11 21:06:40 +0900

    팀목록까지2

--- BaseballM/src/main/java/model/team/Team.java
+++ BaseballM/src/main/java/model/team/Team.java
@@ -1,4 +1,22 @@
 package model.team;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@ToString
+@Getter
 public class Team {
+    private int stadiumIdx;
+    private String teamName;
+    private Timestamp teamCreatedAt;
+
+    @Builder
+    public Team(int stadiumIdx, String teamName, Timestamp teamCreatedAt) {
+        this.stadiumIdx = stadiumIdx;
+        this.teamName = teamName;
+        this.teamCreatedAt = teamCreatedAt;
+    }
 }
